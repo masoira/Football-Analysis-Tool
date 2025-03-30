bd86c0a83a7487f3fdfddaeda0dccd9541e20f4c
Niko V. <nikk0o046@gmail.com>
2025-03-30 20:46:00 +0200

    feat: add Match as SQLModel

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,9 @@ from datetime import datetime
 from pydantic import BaseModel
 from typing import Literal
 
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlmodel import SQLModel, Field, Column
+
 
 class AssistAction(BaseModel):
     x: float
@@ -24,10 +27,11 @@ class Period(BaseModel):
     actions: list[Action]
 
 
-class Match(BaseModel):
-    match_id: str  # UUID created by the frontend
+class Match(SQLModel, table=True):
+    match_id: str = Field(primary_key=True)  # UUID created by the frontend
     match_name: str 
     home_team: str | None = None
     away_team: str | None = None
     date: datetime
-    periods: list[Period]
+    periods: list[dict] = Field(sa_column=Column(JSONB))
+    # TODO: Should user_id be a field here or a part of a separate MatchDb object?
